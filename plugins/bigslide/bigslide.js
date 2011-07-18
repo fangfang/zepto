a437a1d33bfb20431a7004b5206a395fcaab3c5a
zhangyanlu <zhangyanlu@baidu.com>
2011-07-18 15:05:34 +0800

    fix click event

--- plugins/bigslide/bigslide.js
+++ plugins/bigslide/bigslide.js
@@ -1,13 +1,15 @@
 (function($) {
 
-    var REG_TRANS = /(.+,)([^,]+,[^,]+\))$/;
-    function translate(zobj, x, y) {
-        var trans = zobj.css("-webkit-transform");
-        trans = (trans == "none" ? "matrix(1,0,0,1,0,0)" : trans);
-        var m = REG_TRANS.exec(trans);
-        zobj.css("-webkit-transform", m[1] + "" + x + ", " + y + ")");
-    }
+	
+	function parentIfText(node){
+	      return 'tagName' in node ? node : node.parentNode;
+	 }
+	
     
+    function translate(zobj, x, y) {
+        zobj.css("-webkit-transform","translate3d("+x+"px,"+y+"px,0)");
+    }
+
     function power(delta, duration, force) {
         return Math.floor(delta / duration * force);
     }
@@ -49,53 +51,123 @@
             ontouchmove: function() {
             },
             direction: "horizontal",
-            power: 200
+            power: 200,
+			positive: null,
+			negative: null,
+			btnspeed: 80
         }, options), me = this, body = $(document.body);
         
-        me.each(function(index) {
+        return me.css({
+        	"-webkit-perspective":1000,
+        	"-webkit-backface-visibility": 'hidden'
+        }).each(function(index) {
             var thiz = this, that = $(thiz), slider = that.children(), ontouchmove = function(e) {
-                var tmp = getpos(e, opt.direction);
-                last = {
-                    x: last.x + tmp.x - cmove.x,
-                    y: last.y + tmp.y - cmove.y
-                };
-                translate(slider, last.x, last.y);
-                lmove = cmove;
-                cmove = tmp;
-                
-                opt.ontouchmove.call(thiz, cmove, lmove, last);
-            }, ontouchend = function(e) {
-                body.unbind("mousemove", ontouchmove);
-                body.unbind("mouseup", ontouchend);
-                
-                if (opt.power <= 0) {
-                    return;
-                }
-                
-                if (lmove) {
-                    cmove = getpos(e, opt.direction);
-                    var dtime = cmove.time - lmove.time;
-                    
-                    last = computepos(slider, last, cmove, lmove, dtime, opt.power);
-                    
-                    slider.addClass("zepto-bigslide-force");
-                    translate(slider, last.x, last.y);
-                    slider.one("webkitTransitionEnd", function(e) {
-                        slider.removeClass("zepto-bigslide-force");
-                    });
-                }
-            }, cmove, lmove, last = {
+				var tmp = getpos(e.touches[0], opt.direction);
+				last = {
+					x: last.x + tmp.x - cmove.x,
+					y: last.y + tmp.y - cmove.y
+				};
+				translate(slider, last.x, last.y);
+				lmove = cmove;
+				cmove = tmp;
+				
+				opt.ontouchmove.call(thiz, cmove, lmove, last);
+			}, ontouchend = function(e) {
+				
+				body.unbind("touchmove", ontouchmove);
+				body.unbind("touchend", ontouchend);
+				
+				if(triggerClick){
+					$(parentIfText(e.target)).trigger('click');
+				}
+				
+				if (opt.power <= 0) {
+					return;
+				}
+				
+				if (lmove) {
+					cmove = getpos(e.changedTouches[0], opt.direction);
+					var dtime = cmove.time - lmove.time;
+					
+					last = computepos(slider, last, cmove, lmove, dtime, opt.power);
+					
+					slider.addClass("zepto-bigslide-force");
+					translate(slider, last.x, last.y);
+					slider.one("webkitTransitionEnd", function(e) {
+						slider.removeClass("zepto-bigslide-force");
+					});
+				}
+			}, move = function(range) {
+				triggerClick = false;
+				if (opt.direction == "horizontal") {
+		            last.x +=  range;
+		        }
+		        else if (opt.direction == "vertical") {
+		            last.y += range;
+		        }
+				last.x = last.x > 0 ? 0 : last.x;
+		        last.y = last.y > 0 ? 0 : last.y;
+		        
+		        var lcoffset = slider.children().last().offset(), fcoffset = slider.children().first().offset(), poffset = slider.parent().offset(), width = lcoffset.left + lcoffset.width - fcoffset.left, height = lcoffset.top + lcoffset.height - fcoffset.top, pwidth = poffset.width, pheight = poffset.height;
+		        
+		        last.x = (lcoffset.left + lcoffset.width + range) < pwidth ? pwidth - width : last.x;
+		        last.y = (lcoffset.top + lcoffset.height + range) < pheight ? pheight - height : last.y;
+				
+				//slider.addClass('zepto-bigslide-move');
+				translate(slider, last.x, last.y);
+				//slider.one("webkitTransitionEnd", function(e) {
+				//	slider.removeClass('zepto-bigslide-move');
+				//});
+		
+			}, cmove, lmove, last = {
                 x: 0,
                 y: 0
-            };
-            that.bind("mousedown", function(e) {
-                cmove = getpos(e, opt.direction);
+            },triggerClick = true;
+            that.bind("touchstart", function(e) {
+            	triggerClick = true;
+                cmove = getpos(e.touches[0], opt.direction);
                 slider.removeClass("zepto-bigslide-force");
-                body.bind("mousemove", ontouchmove);
-                body.bind("mouseup", ontouchend);
+                body.bind("touchmove", ontouchmove);
+                body.bind("touchend", ontouchend);
                 e.preventDefault();
             });
+			if ( opt.positive ) {
+				var hold = false, ptimeout = function(){
+					if (hold) {
+						move(opt.btnspeed);
+						setTimeout(ptimeout, 100);
+					}
+				};
+				
+				$(opt.positive).bind('touchstart', function(e){
+					e.preventDefault();
+					hold = true;
+					setTimeout(ptimeout, 98);
+				});
+				$(opt.positive).bind('touchend', function(e){
+					e.preventDefault();
+					hold = false;
+				});
+			}
+			if ( opt.negative ) {
+				var hold = false, ntimeout = function(){
+					if (hold) {
+						move(-opt.btnspeed);
+						setTimeout(ntimeout, 100);
+					}
+				};
+				
+				$(opt.negative).bind('touchstart', function(e){
+					e.preventDefault();
+					hold = true;
+					setTimeout(ntimeout, 98);
+				});
+				$(opt.negative).bind('touchend', function(e){
+					e.preventDefault();
+					hold = false;
+				});
+			}
         });
-    }
+    };
     
 })(Zepto);
